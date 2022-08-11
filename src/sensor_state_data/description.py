10b25b42bab8ae40eefcab6ccda891e68227b5bc
J. Nick Koston <nick@koston.org>
2022-08-10 14:04:06 -1000

    fix: preserve backcompat

--- src/sensor_state_data/description.py
+++ src/sensor_state_data/description.py
@@ -14,6 +14,7 @@ class BaseDescription:
     """A base class for all descriptions."""
 
     device_class: BaseDeviceClass | None = None
+    native_unit_of_measurement: Units | None = None
 
 
 @dataclass(frozen=True)
@@ -26,7 +27,6 @@ class BaseBinarySensorDescription(BaseDescription):
 class BaseSensorDescription(BaseDescription):
 
     device_class: SensorDeviceClass | None = None
-    native_unit_of_measurement: Units | None = None
 
 
 @dataclass(frozen=True)
